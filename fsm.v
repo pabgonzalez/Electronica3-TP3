625dabe6b4f13dc6112f219e43249c17b6b2e5fe
pabgonzalez <50333313+pabgonzalez@users.noreply.github.com>
2020-11-19 22:18:57 -0300

    Update fsm.v

--- fsm.v
+++ fsm.v
@@ -1,1 +1,49 @@
+module fsm
+  (
+    input wire CLK, //48MHz
+    input wire SNN,
+    input wire SNS,
+    input wire STH,
+    input wire PNN,
+    input wire PNS,
+    input wire PTH,
+    output reg CS1, //change S1
+    output reg CS2,
+    output reg CS3,
+    output reg CG1,
+    output reg CG2,
+    output reg CG3
+  );
 
+  //Tabla de tiempos del estado 1
+  reg [7:0] time_E1 [3:0];
+  time_E1[0] = 17; //Tabla A
+  time_E1[1] = 17; //Tabla B
+  time_E1[2] = 17; //Tabla C
+  time_E1[3] = 17; //Tabla D
+  
+  reg[7:0] tabla = 'A';
+  reg[7:0] estado = 1;
+  
+  always @ (SNN or SNS or STH)
+    begin
+      if ( SNN & !SNS & !STH & (estado != 5) )
+        begin
+          tabla = 'C';
+          estado = 5; //norton al norte en verde
+        end
+      else if...
+    end
+        
+        
+  //loop de estados
+  always @ (posedge CLK)
+    begin
+      case (estado)
+        1: begin end
+        2:
+        3:
+    end
+          
+endmodule
+ 
