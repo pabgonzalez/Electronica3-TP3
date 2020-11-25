e12c25db74a74d2547d752d712ef4b63e3c34c41
nmestanza <info@nmestanza.com>
2020-11-25 13:12:45 -0300

    Update fsm-v2.v

--- fsm-v2.v
+++ fsm-v2.v
@@ -19,46 +19,50 @@ module fsm
   //Tabla de tiempos del estado 1
     reg [7:0] time_E1 [3:0];
     time_E1[0] = 17; //Tabla A
-    time_E1[1] = 17; //Tabla B
+    time_E1[1] = 3; //Tabla B
     time_E1[2] = 17; //Tabla C
     time_E1[3] = 17; //Tabla D
+
+  //Tabla de tiempos del estado 2
   
     reg [7:0] time_E2 [3:0];
-    time_E2[0] = 17; //Tabla A
-    time_E2[1] = 17; //Tabla B
-    time_E2[2] = 17; //Tabla C
-    time_E2[3] = 17; //Tabla D
+    time_E2[0] = 3; //Tabla A
+    time_E2[1] = 3; //Tabla B
+    time_E2[2] = 3; //Tabla C
+    time_E2[3] = 3; //Tabla D
+
+  //Tabla de tiempos del estado 3
   
     reg [7:0] time_E3 [3:0];
-    time_E3[0] = 17; //Tabla A
-    time_E3[1] = 17; //Tabla B
-    time_E3[2] = 17; //Tabla C
-    time_E3[3] = 17; //Tabla D
+    time_E3[0] = 55; //Tabla A
+    time_E3[1] = 110; //Tabla B
+    time_E3[2] = 27; //Tabla C
+    time_E3[3] = 27; //Tabla D
+
+  //Tabla de tiempos del estado 4
   
     reg [7:0] time_E4 [3:0];
-    time_E4[0] = 17; //Tabla A
-    time_E4[1] = 17; //Tabla B
-    time_E4[2] = 17; //Tabla C
-    time_E4[3] = 17; //Tabla D
+    time_E4[0] = 27; //Tabla A
+    time_E4[1] = 14; //Tabla B
+    time_E4[2] = 14; //Tabla C
+    time_E4[3] = 54; //Tabla D
+
+  //Tabla de tiempos del estado 5
   
     reg [7:0] time_E5 [3:0];
-    time_E5[0] = 17; //Tabla A
-    time_E5[1] = 17; //Tabla B
-    time_E5[2] = 17; //Tabla C
-    time_E5[3] = 17; //Tabla D
-  
-    reg [7:0] time_E6 [3:0];
-    time_E6[0] = 17; //Tabla A
-    time_E6[1] = 17; //Tabla B
-    time_E6[2] = 17; //Tabla C
-    time_E6[3] = 17; //Tabla D
+    time_E5[0] = 24; //Tabla A
+    time_E5[1] = 12; //Tabla B
+    time_E5[2] = 48; //Tabla C
+    time_E5[3] = 12; //Tabla D
+
+
   
     reg[7:0] tabla = 'A';
     reg[7:0] estado = 1;
   
     always @ (SNN or SNS or STH)
         begin
-        //Caso 4
+        //CASO 4
         if ( !SNS & !SNN & STH & (estado != 3) )
             begin
                 tabla = 'B';
@@ -79,11 +83,10 @@ module fsm
             end
       // CASO 1
         else 
-            if(tabla != 'A')
-                begin
-                    tabla = 'A';
-                    estado = estado + 1; 
-                end
+            begin
+                tabla = 'A';
+                estado = estado + 1; 
+            end
         end
         
         
@@ -91,9 +94,45 @@ module fsm
     always @ (posedge CLK, estado)
         begin
             case (estado)
-                1: begin end
+                1:      if (tabla ==)
                 2:
                 3:
+                4:
+                5:
         end
+
+
+
+    //Next-state logic
+	always @ (en, set, change, time_up_R2G, time_up_G2R) 
+		begin              
+			case (e)
+				E_OFF:		if (en) E = (set ? E_RED : E_GREEN);
+							else E = E_OFF;
+				E_RED:		if (en & !change) E = E_RED;  
+							else if (en & change) E = E_YELLOW2G;   
+							else E = E_OFF;
+				E_GREEN:	if (en & !change) E = E_GREEN;  
+							else if (en & change) E = E_YELLOW2R;  
+							else E = E_OFF; 
+				E_YELLOW2G: if (en & time_up_R2G) E = E_GREEN;   
+							else if (en & !time_up_R2G) E = E_YELLOW2G;
+							else E = E_OFF;
+				E_YELLOW2R: if (en & time_up_G2R)  E = E_RED;   
+							else if (en & !time_up_G2R) E = E_YELLOW2R;
+							else E = E_OFF;
+				default:	E = E_ERROR; 
+			endcase 
+			
+		end
+
+        // Transicion al proximo estado (secuencial)
+    always @(negedge resetn, posedge clk)
+        if (resetn == 0) y <= A;
+        else y <= Y;
+
+    // Salida (combinacional)
+    assign z = (y == C);
+endmodule
           
 endmodule
